25d5eb8fa8f823f442fb2ac42c164d9003776f74
unknown <patilrutik501@gmail.com>
2024-03-16 12:47:07 +0530

    done with update

--- src/config/DB_Conn.js
+++ src/config/DB_Conn.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-//const DB_URL= process.env.DBURL;
-const DB_URL =`mongodb://0.0.0.0:27017/smallprojects`
+const DB_URL= process.env.DBURL;
+//const DB_URL =`mongodb://0.0.0.0:27017/smallprojects`
 const Connect_DB =async()=>{
     try {
         await mongoose.connect(DB_URL);
